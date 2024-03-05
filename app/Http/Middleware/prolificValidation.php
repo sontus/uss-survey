b02a0608390ff47ce2205109a9831fbd0e031304
nazmul1010 <nazmulhasan169369@gmail.com>
2024-03-05 10:43:44 +0600

    page 36 created

--- app/Http/Middleware/prolificValidation.php
+++ app/Http/Middleware/prolificValidation.php
@@ -24,9 +24,9 @@ class prolificValidation
         $study_id = Session::get('study_id');
         $session_id = Session::get('session_id');
 
-//        if (empty($prolific_pid) || empty($study_id) || empty($session_id)) {
-//            return redirect()->route('out-from-prolific');
-//        }
+        if (empty($prolific_pid) || empty($study_id) || empty($session_id)) {
+            return redirect()->route('out-from-prolific');
+        }
 
         $userCheck = ProlificUser::where([['prolific_pid', $prolific_pid], ['study_id', $study_id], ['session_id', $session_id]])->first();
         if ($userCheck) {
